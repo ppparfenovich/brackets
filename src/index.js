e40b8503a0ef52be6f3924da1a6a514435ddd534
Pavel <ppparfenovich@gmail.com>
2021-02-26 13:59:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openers = ["{", "[", "("];
+  const closers = ["}", "]", ")"];
+  const match = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+    "|": "|"
+  };
+  
+  var lastOpener = [];
+
+  for (el in str) {
+    if (openers.includes(str[el])) {
+      lastOpener.push(str[el]);
+    }
+    if (closers.includes(str[el])) {
+      const last = lastOpener.pop();
+      if (last !== match[str[el]]) {
+        return false;
+      }
+    }
+  }
+  return lastOpener.length == 0;
+  
+  
+  // let brackets = "[]{}()||"
+  // let arr = []
+
+  // for(let bracket of str) {
+  //   let bracketsIndex = brackets.indexOf(bracket)   //находим индекс скобки в brackets которая = текущей скобки из str
+
+  //   if(bracketsIndex % 2 === 0) {
+  //     arr.push(bracketsIndex + 1)   //добавляем в массив индекс закрывающей скобки для bracket из str
+  //   } else {
+  //     if(arr.length === 0 || arr.pop() !== bracketsIndex) {
+  //       return false;
+  //     }
+  //   }
+  // }
+  // return arr.length === 0
 }
